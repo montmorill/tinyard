d31da5fd6fbaf7e0756189a475fe0e9b0819cc24
montmorill <montmorillonite@qq.com>
2025-01-11 12:05:53 +0800

    :art: refactor operator impl

--- src/ops.rs
+++ src/ops.rs
@@ -1,50 +1,82 @@
-use crate::Value;
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+
 use nalgebra::{ClosedAddAssign, ClosedDivAssign, ClosedSubAssign};
 use num_traits::{One, Zero};
 use simba::scalar::ClosedNeg;
-use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+
+use crate::Value;
 
 macro_rules! binary_op_impl {
-    ($Trait:ident $method:ident $TraitAssign:ident $method_assign:ident $($($bound:tt)+)?) => {
+    (@first $Trait:ident $method:ident $TraitAssign:ident $method_assign:ident) => {
         impl<T: nalgebra::Scalar, const N: usize> $Trait for Value<T, N>
         where
-            T: $Trait<Output = T> + $TraitAssign $(+ $($bound)*)?,
+            T: $Trait<Output = T> + $TraitAssign
         {
-            type Output = Value<T, N>;
+            type Output = Self;
 
             #[inline]
             fn $method(mut self, rhs: Self) -> Self::Output {
-                self.$method_assign(Borrowed {
-                    value: rhs.value,
-                    grad: &rhs.grad,
-                    #[cfg(feature = "hessian")]
-                    hess: &rhs.hess,
-                });
+                self.$method_assign(rhs);
                 self
             }
         }
 
-        impl<T: nalgebra::Scalar, const N: usize> $Trait<&Value<T, N>> for Value<T, N>
+        impl<T: nalgebra::Scalar, const N: usize> $Trait<&Self> for Value<T, N>
         where
-            T: $Trait<Output = T> + $TraitAssign $(+ $($bound)*)?,
+            T: $Trait<Output = T> + $TraitAssign
         {
-            type Output = Value<T, N>;
+            type Output = Self;
 
             #[inline]
             fn $method(mut self, rhs: &Self) -> Self::Output {
-                self.$method_assign(Borrowed {
-                    value: rhs.value.clone(),
-                    grad: &rhs.grad,
-                    #[cfg(feature = "hessian")]
-                    hess: &rhs.hess,
-                });
+                self.$method_assign(rhs);
                 self
             }
         }
 
+        binary_op_impl!(@swap $Trait $method $TraitAssign $method_assign);
+    };
+
+    (@second $Trait:ident $method:ident $TraitAssign:ident $method_assign:ident $($($Bound:tt)+)?) => {
+        impl<T: nalgebra::Scalar, const N: usize> $Trait<&Self> for Value<T, N>
+        where
+            T: $Trait<Output = T> + $TraitAssign $(+ $($Bound)+)?
+        {
+            type Output = Self;
+
+            #[inline]
+            fn $method(self, rhs: &Self) -> Self::Output {
+                self.$method(rhs.clone())
+            }
+        }
+
+        binary_op_impl!(@swap $Trait $method $TraitAssign $method_assign $($($Bound)+)?);
+
+        impl<T: nalgebra::Scalar, const N: usize> $TraitAssign for Value<T, N>
+        where
+            T: $Trait<Output = T> + $TraitAssign $(+ $($Bound)+)?
+        {
+            #[inline]
+            fn $method_assign(&mut self, rhs: Self) {
+                *self = (&*self).$method(rhs);
+            }
+        }
+
+        impl<T: nalgebra::Scalar, const N: usize> $TraitAssign<&Self> for Value<T, N>
+        where
+            T: $Trait<Output = T> + $TraitAssign $(+ $($Bound)+)?
+        {
+            #[inline]
+            fn $method_assign(&mut self, rhs: &Self) {
+                self.$method_assign(rhs.clone());
+            }
+        }
+    };
+
+    (@swap $Trait:ident $method:ident $TraitAssign:ident $method_assign:ident $($($Bound:tt)+)?) => {
         impl<T: nalgebra::Scalar, const N: usize> $Trait<Value<T, N>> for &Value<T, N>
         where
-            T: $Trait<Output = T> + $TraitAssign $(+ $($bound)*)?,
+            T: $Trait<Output = T> + $TraitAssign $(+ $($Bound)+)?
         {
             type Output = Value<T, N>;
 
@@ -53,44 +85,7 @@ macro_rules! binary_op_impl {
                 rhs.$method(self)
             }
         }
-
-        impl<T: nalgebra::Scalar, const N: usize> $TraitAssign for Value<T, N>
-        where
-            T: $Trait<Output = T> + $TraitAssign $(+ $($bound)*)?,
-        {
-            #[inline]
-            fn $method_assign(&mut self, rhs: Self) {
-                self.$method_assign(Borrowed {
-                    value: rhs.value,
-                    grad: &rhs.grad,
-                    #[cfg(feature = "hessian")]
-                    hess: &rhs.hess,
-                });
-            }
-        }
-
-        impl<T: nalgebra::Scalar, const N: usize> $TraitAssign<&Self> for Value<T, N>
-        where
-            T: $Trait<Output = T> + $TraitAssign $(+ $($bound)*)?,
-        {
-            #[inline]
-            fn $method_assign(&mut self, rhs: &Self) {
-                self.$method_assign(Borrowed {
-                    value: rhs.value.clone(),
-                    grad: &rhs.grad,
-                    #[cfg(feature = "hessian")]
-                    hess: &rhs.hess,
-                });
-            }
-        }
-    };
-}
-
-struct Borrowed<'a, T: nalgebra::Scalar, const N: usize> {
-    value: T,
-    grad: &'a nalgebra::SVector<T, N>,
-    #[cfg(feature = "hessian")]
-    hess: &'a nalgebra::SMatrix<T, N, N>,
+    }
 }
 
 impl<T: nalgebra::Scalar, const N: usize> Neg for Value<T, N>
@@ -109,11 +104,11 @@ where
     }
 }
 
-impl<'a, T: nalgebra::Scalar, const N: usize> AddAssign<Borrowed<'a, T, N>> for Value<T, N>
+impl<'a, T: nalgebra::Scalar, const N: usize> AddAssign for Value<T, N>
 where
     T: ClosedAddAssign,
 {
-    fn add_assign(&mut self, rhs: Borrowed<'a, T, N>) {
+    fn add_assign(&mut self, rhs: Self) {
         self.value += rhs.value;
         self.grad += rhs.grad;
         #[cfg(feature = "hessian")]
@@ -123,11 +118,26 @@ where
     }
 }
 
-impl<'a, T: nalgebra::Scalar, const N: usize> SubAssign<Borrowed<'a, T, N>> for Value<T, N>
+impl<T: nalgebra::Scalar, const N: usize> AddAssign<&Self> for Value<T, N>
+where
+    T: ClosedAddAssign,
+{
+    fn add_assign(&mut self, rhs: &Self) {
+        self.value += rhs.value.clone();
+        self.grad += &rhs.grad;
+        #[cfg(feature = "hessian")]
+        {
+            self.hess += &rhs.hess;
+        }
+    }
+}
+
+binary_op_impl!(@first Add add AddAssign add_assign);
+impl<T: nalgebra::Scalar, const N: usize> SubAssign for Value<T, N>
 where
     T: ClosedSubAssign,
 {
-    fn sub_assign(&mut self, rhs: Borrowed<'a, T, N>) {
+    fn sub_assign(&mut self, rhs: Self) {
         self.value -= rhs.value;
         self.grad -= rhs.grad;
         #[cfg(feature = "hessian")]
@@ -137,63 +147,73 @@ where
     }
 }
 
-impl<'a, T: nalgebra::Scalar, const N: usize> MulAssign<Borrowed<'a, T, N>> for Value<T, N>
+impl<T: nalgebra::Scalar, const N: usize> SubAssign<&Self> for Value<T, N>
 where
+    T: ClosedSubAssign,
+{
+    fn sub_assign(&mut self, rhs: &Self) {
+        self.value -= rhs.value.clone();
+        self.grad -= &rhs.grad;
+        #[cfg(feature = "hessian")]
+        {
+            self.hess -= &rhs.hess;
+        }
+    }
+}
+
+binary_op_impl!(@first Sub sub SubAssign sub_assign);
+
+impl<T: nalgebra::Scalar, const N: usize> Mul for Value<T, N>
+where
+    // `Zero` and `One` imply a `Closed` verison of `AddAssign` and `MulAssign`.
     T: AddAssign + MulAssign + Zero + One,
 {
-    fn mul_assign(&mut self, rhs: Borrowed<'a, T, N>) {
-        let lhs = std::mem::replace(self, unsafe {
-            std::mem::MaybeUninit::<_>::uninit().assume_init()
-        });
-        let mut manual = std::mem::ManuallyDrop::new(self);
-        manual.value = lhs.value.clone() * rhs.value.clone();
-        #[cfg(not(feature = "hessian"))]
-        {
-            manual.grad = lhs.grad * rhs.value + rhs.grad * lhs.value;
-        }
-        #[cfg(feature = "hessian")]
-        {
-            manual.grad = lhs.grad.clone() * rhs.value.clone() + rhs.grad * lhs.value.clone();
-            manual.hess = lhs.hess * rhs.value
-                + rhs.grad * lhs.grad.transpose()
-                + lhs.grad * rhs.grad.transpose()
-                + rhs.hess * lhs.value;
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Value {
+            value: self.value.clone() * rhs.value.clone(),
+            #[cfg(feature = "hessian")]
+            hess: self.hess * rhs.value.clone()
+                + self.grad.clone() * rhs.grad.transpose()
+                + rhs.grad.clone() * self.grad.transpose()
+                + rhs.hess * self.value.clone(),
+            grad: self.grad * rhs.value + rhs.grad * self.value,
         }
     }
 }
 
-impl<'a, T: nalgebra::Scalar, const N: usize> DivAssign<Borrowed<'a, T, N>> for Value<T, N>
+binary_op_impl!(@second Mul mul MulAssign mul_assign AddAssign + Zero + One);
+
+impl<T: nalgebra::Scalar, const N: usize> Div for Value<T, N>
 where
+    // `Zero` and `One` imply a `Closed` verison of `AddAssign` and `MulAssign`.
     T: AddAssign + ClosedSubAssign + MulAssign + ClosedDivAssign + Zero + One,
 {
-    fn div_assign(&mut self, rhs: Borrowed<'a, T, N>) {
-        let lhs = std::mem::replace(self, unsafe {
-            std::mem::MaybeUninit::<_>::uninit().assume_init()
-        });
-        let mut manual = std::mem::ManuallyDrop::new(self);
-        manual.value = lhs.value.clone() / rhs.value.clone();
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        let value = self.value.clone() / rhs.value.clone();
         #[cfg(not(feature = "hessian"))]
-        {
-            manual.grad = (lhs.grad * rhs.value.clone() - rhs.grad * lhs.value)
-                / (rhs.value.clone() * rhs.value);
-        }
+        let grad = (self.grad * rhs.value.clone() - rhs.grad * self.value)
+            / (rhs.value.clone() * rhs.value);
         #[cfg(feature = "hessian")]
-        {
-            manual.grad = (lhs.grad * rhs.value.clone() - rhs.grad * lhs.value)
-                / (rhs.value.clone() * rhs.value.clone());
-            manual.hess = (lhs.hess
-                - manual.grad.clone() * rhs.grad.transpose()
-                - rhs.grad * manual.grad.transpose()
-                - rhs.hess * manual.value.clone())
-                / rhs.value;
+        let grad = (self.grad * rhs.value.clone() - rhs.grad.clone() * self.value)
+            / (rhs.value.clone() * rhs.value.clone());
+        Value {
+            #[cfg(feature = "hessian")]
+            hess: (self.hess
+                - grad.clone() * rhs.grad.transpose()
+                - rhs.grad * grad.transpose()
+                - rhs.hess * value.clone())
+                / rhs.value,
+            value,
+            grad,
         }
     }
 }
 
-binary_op_impl!(Add add AddAssign add_assign);
-binary_op_impl!(Sub sub SubAssign sub_assign);
-binary_op_impl!(Mul mul MulAssign mul_assign Zero + One + AddAssign);
-binary_op_impl!(Div div DivAssign div_assign Zero + One + AddAssign + ClosedSubAssign + MulAssign);
+binary_op_impl!(@second Div div DivAssign div_assign AddAssign + ClosedSubAssign + MulAssign + Zero + One);
 
 impl<T: nalgebra::Scalar, const N: usize> Value<T, N> {
     fn chain(
@@ -203,19 +223,13 @@ impl<T: nalgebra::Scalar, const N: usize> Value<T, N> {
         #[cfg(feature = "hessian")] hess: T, // ddf/dxx
     ) -> Self
     where
-        T: Zero + One + AddAssign + MulAssign,
+        T: AddAssign + MulAssign + Zero + One,
     {
-        #[cfg(not(feature = "hessian"))]
-        return Self {
+        Self {
             value,
+            #[cfg(feature = "hessian")]
+            hess: &self.grad * self.grad.transpose() * hess + &self.hess * grad.clone(),
             grad: &self.grad * grad,
-        };
-
-        #[cfg(feature = "hessian")]
-        return Self {
-            value,
-            grad: &self.grad * grad.clone(),
-            hess: &self.grad * self.grad.transpose() * hess + &self.hess * grad,
-        };
+        }
     }
 }
